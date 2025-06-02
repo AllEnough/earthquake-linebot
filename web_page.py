2d7ed9399e1a892640720ab680afd4671880f859
AllEnough <138246704+AllEnough@users.noreply.github.com>
2025-06-03 00:46:40 +0800

    update

--- web_page.py
+++ web_page.py
@@ -67,6 +67,7 @@ def index():
         summary=summary,
         line_help=line_help,
         now=now,
+        timedelta=timedelta,  # ✅ 加這行
         show_heatmap_button=True  # ✅ 傳給模板啟用熱力圖按鈕
     )
 
